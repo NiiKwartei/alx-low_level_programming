e4c56fd91fae68c8690b143db4470712d254aeb9
NiiKwartei <winstonquartey@gmail.com>
2023-02-25 17:55:55 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print each alphabet 10 times in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, k;
+
+	for (k = 0; k < 10; k++)
+	{
+
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
